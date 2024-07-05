10ce2c56c79070b361efce89a787afb1b4152876
manydots <xuys@genergy-tech.cn>
2024-07-05 11:19:03 +0800

    变更枚举定义类型

--- src/hook/HookType.ts
+++ src/hook/HookType.ts
@@ -7,11 +7,11 @@ import _HookNative from './HookNative'; // hook NativeFunction
  *  @param Leave_GameWord 玩家下线
  *
  */
-const _HookType = {
-    History_Log: 0x854f990, // 捕获玩家游戏日志
-    Reach_GameWord: 0x86c4e50, // 玩家上线
-    Leave_GameWord: 0x86c5288 // 玩家下线
-};
+enum _HookType {
+    History_Log = 0x854f990, // 捕获玩家游戏日志
+    Reach_GameWord = 0x86c4e50, // 玩家上线
+    Leave_GameWord = 0x86c5288 // 玩家下线
+}
 
 export const HookNative = _HookNative;
 export const HookType = _HookType;
